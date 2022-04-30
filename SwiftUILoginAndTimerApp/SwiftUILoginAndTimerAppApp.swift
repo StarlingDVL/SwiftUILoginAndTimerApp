888cedf285ae352bc75f95096ffc2328816aff30
ISkvor <starlingy608@gmail.com>
2022-04-30 12:28:29 +0300

    Initial Commit

---
+++ SwiftUILoginAndTimerApp/SwiftUILoginAndTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginAndTimerAppApp.swift
+//  SwiftUILoginAndTimerApp
+//
+//  Created by Юрий Скворцов on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginAndTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
